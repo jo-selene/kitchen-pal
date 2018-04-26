4ebc9a332f6cfebe32c298311764add3f23cf8aa
jo-selene <jmperez0011@gmail.com>
2018-04-25 21:28:21 -0700

    Adding a test case for MyKitchen model setup.

--- KitchenPal/MyKitchen/tests.py
+++ KitchenPal/MyKitchen/tests.py
@@ -4,12 +4,18 @@ from .models import MyKitchen, Food
 
 # Create your tests here.
 
-'''class MyKitchenModelTestCase(TestCase):
+class MyKitchenModelTestCase(TestCase):
     """ This class defines the test suite for the MyKitchen model """
 
     def setUp(self):
-        """ Test the """
-        self.Items =
+        """ Test the creation of a users kitchen. This will mean all the food items that the
+            user currently has in their home."""
         
-'''
+        self.bananas = Food(1,"Fruit",1,"Week","Fridge","Keep in fridge before they get brown spots")
+        self.chicken = Food(2,"Poultry",3,"Months","Freezer","Keep each chicken breast in its own ziplock bag and freeze")
+        self.items = [self.bananas,self.chicken]
+        self.ID = "User ID"
+        self.kitchen = MyKitchen(ID=self.ID, Items=self.items)
+        
+
 
