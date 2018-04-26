2cb29062859de818e769fa7da04d4b561fce6d09
jo-selene <jmperez0011@gmail.com>
2018-04-25 21:13:12 -0700

    Adding MyKitchen application to project. Still need to create test cases and model.

---
+++ KitchenPal/MyKitchen/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .models import MyKitchen, Food
+
+
+# Create your tests here.
+
+'''class MyKitchenModelTestCase(TestCase):
+    """ This class defines the test suite for the MyKitchen model """
+
+    def setUp(self):
+        """ Test the """
+        self.Items =
+        
+'''
+
