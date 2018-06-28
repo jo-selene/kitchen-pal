d2b212ea1be49fc058d0310f7b153ec17630e688
jo-selene <jmperez0011@gmail.com>
2018-06-27 21:06:28 -0700

    Playing around with views and templates in Django for the User view.

--- KitchenPal/User/urls.py
+++ KitchenPal/User/urls.py
@@ -1,6 +1,6 @@
-# File Name:        User/views.py
-# Creation Date:    5/20/2018
-# Last Edited:      5/20/2018
+# File Name:        User/urls.py
+# Creation Date:    5/27/2018
+# Last Edited:      5/27/2018
 # Author(s):        Jocelyne Perez
 #
 # Purpose:          The purpose of this file is to create url's for the User model views.
@@ -9,10 +9,11 @@
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateUserView #, DetailsView
+from .views import CreateUserView, index
 
 urlpatterns = {
     url('createuser$', CreateUserView.as_view(),name='create'),
+    url('$', index, name='index')
    # url('user/(?P<pk>[0-9]+)/$', DetailsView.as_view(),name='details')
 }
 
--- KitchenPal/User/views.py
+++ KitchenPal/User/views.py
@@ -9,12 +9,24 @@
 
 
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 from rest_framework import generics
 from .models import User
 from .serializer import UserSerializer
 # Create your views here.
 
 
+def index(request):
+	# In a sanbox phase, next step is to remove the current code in index and replace it with the creation/log-in of users
+	all_users = User.objects.all()
+	template = loader.get_template('User/index.html')
+	context = {
+		'all_users' : all_users,
+
+	}
+	return HttpResponse(template.render(context,request))
+	
 class CreateUserView(generics.CreateAPIView):
 	""" This class defines the create (POST) behaviour of our API """
 
